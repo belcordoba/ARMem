2b00f60ea275189d0619d6cb506eaebeaa850c7a
Belén Córdoba <belencoar2006@gmail.com>
2024-03-15 19:32:13 -0600

    Updated functions
    Some improvements were done to the player_registration, levels, restart and menu functions for efficiency.
    
    Co-Authored-By: Mariangel2828 <162812751+Mariangel2828@users.noreply.github.com>

--- app.py
+++ app.py
@@ -11,34 +11,37 @@ def user_names(dic_players):
     list_base = [0, 0, 0]
     player_amount = 0
     player_list = []
-    print("#### Player registration: ####")
-    if len(dic_players) > 0:
-        for name in dic_players.keys():
-            player_list.append(name)
-        print(f"Current player list:\n{player_list}")
-
+    for player in dic_players.keys():
+        player_list.append(player)
     while int(player_amount) <= 0:
+        print('\033[2J')
+        print("#### Player registration: ####")
         player_amount = input("Enter the amount of players that you want to add: ")
         try:
             int_check = int(player_amount)
         except ValueError:
-            print("ERROR: That's not a valid number! Make sure to enter an integer.")
+            cont = input("ERROR: That's not a valid amount! Make sure to enter an integer.\nPress the enter key to try again.\n")
             player_amount = 0
         else:
             if int(player_amount) <= 0:
-                print("ERROR: The amount of players must be 1 or above!")
+                cont = input("ERROR: The amount of players must be 1 or above!\nPress the enter key to try again.\n")
+    print('\033[2J')
 
     for i in range(int(player_amount)):
+        print("#### Player registration: ####")
+        print(f"Current player list:\n{player_list}")
         name = input(f"Enter player #{i+1}'s name: ")
         while name in dic_players:
-            name = input(f"There is another player with that name! Use a different name.\nEnter player #{i+1}'s name: ")
+            cont = input(f"There is another player with that name! Use a different name.\nPress the enter key to try again.\n")
+            print('\033[2J')
+            name = input(f"#### Player registration: ####\nCurrent player list:\n{player_list}\nEnter player #{i+1}'s name: ")
         list_times = list_base[:]
         dic_players.setdefault(name, list_times)
-
-    for name in dic_players.keys():
-        if name not in player_list:
-            player_list.append(name)
-    cont = input(f"Player registration completed!\nCurrent player list:\n{player_list}\nPress the enter key to continue.\n")
+        for player in dic_players.keys():
+            if player not in player_list:
+                player_list.append(player)
+        print('\033[2J')
+    cont = input(f"\n#### Player registration: ####\nPlayer registration completed!\nCurrent player list:\n{player_list}\nPress the enter key to continue.\n")
     print('\033[2J')
 
 def generate_list(length):
@@ -52,7 +55,6 @@ def generate_list(length):
     """
     options = [(0, "pineapple"), (1, "cherry"), (2, "grape"), (3, "pear"), (4, "soursop")]
     generated_list = []
-
     while len(generated_list) != length:
         number = random.choice(options)
         if number not in generated_list:
@@ -81,11 +83,10 @@ def levels(level, players):
         elif level == 3:
             cont = input("Level 3:\nDid you think that was all? For this level, you will have to arrange a total of five markers.\nReady to start?\n\nPress the enter key to continue.\n")
         print('\033[2J')
-
         i = -1 + level
         for iteration in range(1, 6):
             print(f"Round {iteration} is about to start!")
-            #time.sleep(2)
+            time.sleep(2)
             for player in players.keys():
                 elements = generate_list(i+3)
                 name_elements = []
@@ -98,19 +99,14 @@ def levels(level, players):
                 time.sleep(2)
                 print(f"Memorize the following elements:\n{name_elements}")
                 time.sleep(2)
-                print("3...")
-                time.sleep(1)
-                print("2...")
-                time.sleep(1)
-                print("1...")
-                time.sleep(1)
+                for i in (range(0,4)):
+                        print(f"{3-i}...")
+                        time.sleep(1)
                 print("Go!")
                 time.sleep(1)
                 print('\033[2J')
-
-                iteration_time = round(ar.start_sorting(id_elements,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=True), 2)
+                iteration_time = round(ar.start_sorting(id_elements,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False), 2)
                 players[player][i] += iteration_time
-
                 print(f"Great job, {player}!")
                 time.sleep(2)
                 print('\033[2J')
@@ -139,7 +135,6 @@ def level_report(level, data):
         length = len(data)
         for x, y in data.items():
             data_list.append([x,y[level-1]])
-
         data_list.sort(key=sort_value)
         print(f"#### Leaderboard - Level #{level-0} ####")
         for i in range(length):
@@ -158,7 +153,6 @@ def game_report(data):
         data_sum = y[0] + y[1] + y[2]
         data_list.append([x,data_sum])
         data_sum = 0
-
     data_list.sort(key=sort_value)
     print(f"#### Leaderboard - Full game ####")
     for i in range(length):
@@ -175,7 +169,6 @@ def restart():
             print("#### Thank you for playing ARMem #### \nWould you like to play again? If you do, you will return to the main menu.\n1 > Yes, using a new player list\n2 > Yes, using the same player list\n3 > No")
             player_choice = str(input("Enter the number of the option that you would like to choose: "))
             print('\033[2J')
-
             if player_choice == "1":
                 dic_players = {}
                 cont = input("Player list cleared!\nYou will be able to add new players with the player registration menu.\nPress the enter key to continue.\n")
@@ -188,8 +181,7 @@ def restart():
             elif player_choice == "3":
                 quit()
             else:
-                print("This is not a valid choice! Enter the number of one of the options.\nReturning to end screen...")
-                time.sleep(4)
+                cont = input("This is not a valid choice! Enter the number of one of the options.\nPress the enter key to return to the end screen.\n")
                 print('\033[2J')   
 
 def game():
@@ -212,21 +204,18 @@ def main_menu(dic_players):
         print("#### ARMem #### \n1 > Player registration\n2 > Start game\n3 > Leave game")
         player_choice = str(input("Enter the number of the option that you would like to choose: "))
         print('\033[2J')
-        
         if player_choice == "1":
             user_names(dic_players)
         elif player_choice == "2":
             if len(dic_players) > 0:
                 game()
             else:
-                print("There needs to be at least one player to start the game!\nReturning to main menu...")
-                time.sleep(3)
+                cont = input("There needs to be at least one player to start the game!\nPress the enter key to return to the main menu.\n")
                 print('\033[2J')
         elif player_choice == "3":
             quit()
         else:
-            print("This is not a valid choice! Enter the number of one of the options.\nReturning to main menu...")
-            time.sleep(4)
+            cont = input("This is not a valid choice! Enter the number of one of the options.\nPress the enter key to return to the main menu.\n")
             print('\033[2J')
 
 dic_players = {}
